52d0cca248b17c455f117ce44bb51f85b742f195
Németh Balázs <nemeth.balazs.19f@szlgbp.com>
2022-03-25 13:02:54 +0100

    urls.py bigyózás

--- PROJEKT/PROJEKT/urls.py
+++ PROJEKT/PROJEKT/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from APP.views import felveteliview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
