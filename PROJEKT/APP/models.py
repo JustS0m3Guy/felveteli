c63006616ad5a80802f602328b2c816d4e8f3c3a
Németh Balázs <nemeth.balazs.19f@szlgbp.com>
2022-03-25 12:57:31 +0100

    APP cuccok

--- PROJEKT/APP/models.py
+++ PROJEKT/APP/models.py
@@ -1,3 +1,17 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    class Meta:
+
+        verbose_name = 'student'
+        verbose_name_plural = 'students'
+
+    student_name = models.CharField(max_length=255)
+    student_id = models.CharField(max_length=255)
+    student_points = models.FloatField()
+    student_acceptence = models.BooleanField()
+    
+    def __str__(self):
+        pass
