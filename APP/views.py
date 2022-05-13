0f67014bc98928f40d22f9fc8da75968d759c42b
NM <balazs200428@gmail.com>
2022-05-13 19:22:37 +0200

    DONE

--- APP/views.py
+++ APP/views.py
@@ -7,10 +7,24 @@ def index(request):
     template="index.html"
     context={}
     if request.method=="POST":
+        english_accetpance = 85
+        math_accetpance = 90
+        it_accetpance = 70
+        italian_acceptance = 75
+        bioligy_accetpance = 50
         diakok = list(Student.objects.filter(student_id=request.POST['studentid']))
-        print(len(diakok))
-        if(len(diakok) == 1):
+        if(len(diakok) >= 1):
             template="results.html"
+            context={
+                'studentname' : diakok[0].student_name,
+                'studentid' : diakok[0].student_id,
+                'studentpoints' : diakok[0].student_points,
+                'english' : diakok[0].student_points >= english_accetpance,
+                'math' : diakok[0].student_points >= math_accetpance,
+                'it' : diakok[0].student_points >= it_accetpance,
+                'italian' : diakok[0].student_points >= italian_acceptance,
+                'bioligy' : diakok[0].student_points >= bioligy_accetpance,
+            }
         else:
             context={'incorrect' : len(diakok) != 1}
     return render(request, template, context)
@@ -19,11 +33,6 @@ def results(request):
     template="results.html"
     context={}
     return render(request, template, context)
-    #english_accetpance = models.BooleanField(default=False)
-    #math_accetpance = models.BooleanField(default=False)
-    #it_accetpance = models.BooleanField(default=False)
-    #english_accetpance = models.BooleanField(default=False)
-    #italian_acceptance = models.BooleanField(default=False)
 
 def import_data(request):
     template="import.html"
@@ -33,9 +42,10 @@ def import_data(request):
         import_data = list(request.POST['input_data'].split(';'))
         if(len(import_data) == 3):
             Student.objects.create(student_name = import_data[0],student_id = import_data[1],student_points = import_data[2])
-            possible_states+=1
+            possible_states==1
             context={'possible_states' : possible_states}
         else:
+            possible_states==2
             context={'possible_states' : possible_states}
         
         
